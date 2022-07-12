6fd5074166f5e70249a8869959a23c41b145efaf
hiteshk5 <69790258+hiteshk5@users.noreply.github.com>
2022-07-12 21:45:27 +0530

    Upload match end scrapping

--- scrapper.py
+++ scrapper.py
@@ -24,19 +24,25 @@ try:
     inningsScore = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/h2")))
     status = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/div[2]")))
     batsmanOneName = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/a")))
-    batsmanOneRuns = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]")))
-    batsmanOneBalls = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]")))
-    batsmanTwoName = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[1]/a")))
-    batsmanTwoRuns = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[2]")))
-    batsmanTwoBalls = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[3]")))
-    bowlerName =  WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/a")))
-    bowlerOvers = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[2]")))
-    bowlerRuns = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[4]")))
-    bowlerWickets = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[5]")))
-    message = inningsScore.text+ "\n" + status.text + "\nBatting:\n" + batsmanOneName.text + "* " + batsmanOneRuns.text + " (" + batsmanOneBalls.text + ")\n" + batsmanTwoName.text + " " + batsmanTwoRuns.text + " (" + batsmanTwoBalls.text + ")\nBowling:\n" + bowlerName.text + " O: " + bowlerOvers.text +" R:" + bowlerRuns.text + " W: " + bowlerWickets.text
+    if batsmanOneName.text != "":
+        batsmanOneRuns = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]")))
+        batsmanOneBalls = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]")))
+        batsmanTwoName = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[1]/a")))
+        batsmanTwoRuns = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[2]")))
+        batsmanTwoBalls = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[3]")))
+        bowlerName =  WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/a")))
+        bowlerOvers = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[2]")))
+        bowlerRuns = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[4]")))
+        bowlerWickets = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[5]")))
+        message = inningsScore.text+ "\n" + status.text + "\nBatting:\n" + batsmanOneName.text + "* " + batsmanOneRuns.text + " (" + batsmanOneBalls.text + ")\n" + batsmanTwoName.text + " " + batsmanTwoRuns.text + " (" + batsmanTwoBalls.text + ")\nBowling:\n" + bowlerName.text + " O: " + bowlerOvers.text +" R:" + bowlerRuns.text + " W: " + bowlerWickets.text
+    #Match Over Scorecard
+    else:
+        firstInningsScore = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[2]/div[1]/div[1]/h2[1]")))
+        secondInningsScore = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[2]/div[1]/div[1]/h2[2]")))
+        status = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[2]/div[4]/div[3]/div[2]/div[2]/div[2]")))
+        message = firstInningsScore.text + "\n" + secondInningsScore.text + "\n" + status.text
 except:
     message = team + " is not playing a match right now."
 #Closing the Browser
 print(message)
 browser.close()
-
